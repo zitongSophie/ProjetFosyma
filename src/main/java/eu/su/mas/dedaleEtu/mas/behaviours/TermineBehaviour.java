42a6fedb2bb13d7e683f92e3cc0c4557fbfc4fe2
zitongSophie <zitong.sohpie@gmail.com>
2021-04-29 19:28:21 +0200

    add

---
+++ src/main/java/eu/su/mas/dedaleEtu/mas/behaviours/TermineBehaviour.java
@@ -0,0 +1,14 @@
+package eu.su.mas.dedaleEtu.mas.behaviours;
+
+import jade.core.behaviours.OneShotBehaviour;
+
+public class TermineBehaviour extends OneShotBehaviour {
+
+	@Override
+	public void action() {
+		// TODO Auto-generated method stub
+		System.out.println(this.myAgent.getLocalName()+"~~~~~~ moveAlone termine~~~~~~~~~~~");
+
+	}
+
+}
